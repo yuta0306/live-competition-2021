dc7f6d59efa8e5c38902a0237efc3b24495c3c5c
yuta0306 <you-2001-3-6@ezweb.ne.jp>
2022-08-05 17:24:46 +0900

    deploy

--- app.py
+++ app.py
@@ -11,15 +11,15 @@ debug = False
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "secret!"
 
-# model = load_bot(
-#     df_context_path="./datasets/clusterdf_context.csv",
-#     df_uttr_path="./datasets/clusterdf_uttr.csv",
-#     model_name="cl-tohoku/bert-base-japanese",
-#     tsne_context_path="./datasets/tsne_context.pkl",
-#     tsne_uttr_path="./datasets/tsne_uttr.pkl",
-#     max_length=32,
-#     threshold=0.7,
-# )
+model = load_bot(
+    df_context_path="./datasets/clusterdf_context.csv",
+    df_uttr_path="./datasets/clusterdf_uttr.csv",
+    model_name="cl-tohoku/bert-base-japanese",
+    tsne_context_path="./datasets/tsne_context.pkl",
+    tsne_uttr_path="./datasets/tsne_uttr.pkl",
+    max_length=32,
+    threshold=0.7,
+)
 
 
 @app.context_processor
@@ -39,7 +39,7 @@ def dated_url_for(endpoint, **values):
 @app.route("/")
 def home():
     id_ = secrets.token_urlsafe(16)
-    # model.register_chat_id(id_)
+    model.register_chat_id(id_)
     session.update({"id": id_})
     return render_template("index.html")
 
@@ -51,10 +51,10 @@ def message():
     data = data.decode("utf-8").split(";")
 
     context = " [SEP] ".join(data[-2:])
-    # reply = model.reply(context, id_)
-    # print(reply)
-    # return {"context": context, "reply": reply, "history": data}
-    return {"context": context, "history": data}
+    reply = model.reply(context, id_)
+    print(reply)
+    return {"context": context, "reply": reply, "history": data}
+    # return {"context": context, "history": data}
 
 
 if __name__ == "__main__":
