771128d0b51a859738d1aab01b5039d5799fa7f3
yuta0306 <you-2001-3-6@ezweb.ne.jp>
2022-08-05 15:16:39 +0900

    release

--- Procfile
+++ Procfile
@@ -1,1 +1,4 @@
+release: pip install ipadic==1.0.0
+release: pip install mecab-python3==1.0.4
+release: pip install torch==1.11.0
 web: python app.py
--- app.py
+++ app.py
@@ -1,16 +1,25 @@
 import os
 import secrets
-import subprocess
 
 from flask import Flask, render_template, request, session
 from flask.helpers import url_for
+
+from src.backbone import load_bot
 
 debug = False
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "secret!"
 
-model = None
+model = load_bot(
+    df_context_path="./datasets/clusterdf_context.csv",
+    df_uttr_path="./datasets/clusterdf_uttr.csv",
+    model_name="cl-tohoku/bert-base-japanese",
+    tsne_context_path="./datasets/tsne_context.pkl",
+    tsne_uttr_path="./datasets/tsne_uttr.pkl",
+    max_length=32,
+    threshold=0.7,
+)
 
 
 @app.context_processor
@@ -27,34 +36,10 @@ def dated_url_for(endpoint, **values):
     return url_for(endpoint, **values)
 
 
-def load(id_: str):
-    subprocess.run(["bash", "run.sh"])
-    from src.backbone import load_bot
-
-    model = load_bot(
-        df_context_path="./datasets/clusterdf_context.csv",
-        df_uttr_path="./datasets/clusterdf_uttr.csv",
-        model_name="cl-tohoku/bert-base-japanese",
-        tsne_context_path="./datasets/tsne_context.pkl",
-        tsne_uttr_path="./datasets/tsne_uttr.pkl",
-        max_length=32,
-        threshold=0.7,
-    )
-    model.register_chat_id(id_)
-    return model
-
-
 @app.route("/")
 def home():
-    global model
     id_ = secrets.token_urlsafe(16)
-    if model is None:
-        model = load(id_)
-        print("first loaded")
-        print(model)
-        print(dir(model))
-    else:
-        model.register_chat_id(id_)
+    model.register_chat_id(id_)
     session.update({"id": id_})
     return render_template("index.html")
 
